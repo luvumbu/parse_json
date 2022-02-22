77278929c2200636138770135c6d48b1f148a6dd
luvumbu <luvumbu.n@gmail.com>
2022-02-22 04:49:01 +0100

    mode 2

--- index.php
+++ index.php
@@ -12,14 +12,14 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     var myObj = JSON.parse(this.responseText);
-    document.getElementById("demo").innerHTML = myObj.name;
+    document.getElementById("demo").innerHTML = myObj[1].name;
   }
 };
-xmlhttp.open("GET", "demo.txt", true);
+xmlhttp.open("GET", "demo.json", true);
 xmlhttp.send();
 </script>
 
-<p>Take a look at <a href="demo.txt" target="_blank">json_demo.txt</a></p>
+<p>Take a look at <a href="demo.json" target="_blank">json_demo.txt</a></p>
 
 </body>
 </html>
